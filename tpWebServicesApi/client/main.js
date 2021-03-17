7554c91f12960e9bb204f1a839b1b5ffc3f4ce01
bentarr <benetierpaul@gmail.com>
2021-03-17 16:03:34 +0100

    Add http

--- tpWebServicesApi/client/main.js
+++ tpWebServicesApi/client/main.js
@@ -1,22 +1,26 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { HTTP } from 'meteor/http'; 
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.home.onCreated(function homeOnCreated() {
+  let ctrl = this;
+  this.movies = new ReactiveVar();
+  
+  HTTP.call(
+    'GET',
+    'http://localhost:3000/api/discover/movies', 
+  {},
+  (error, response) => {
+    ctrl.movies.set(
+      JSON.parse(response.content).results)
+    }
+  );
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  Template.home.helpers({
+  movies() {
+  return Template.instance().movies.get();
+  }
 });
--- tpWebServicesApi/server/main.js
+++ tpWebServicesApi/server/main.js
@@ -1,5 +1,22 @@
 import { Meteor } from 'meteor/meteor';
+import { SERVER_CONFIG } from './server-config';
+import { WebApp } from 'meteor/webapp';
+import { HTTP } from 'meteor/http';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(() => {});
+
+WebApp.connectHandlers.use('/api/discover/movies', (req, res, next) => {
+  let baseurl = SERVER_CONFIG.themoviedb_api_config.base_url;
+  let apikey = SERVER_CONFIG.themoviedb_api_config.api_key;
+  let language = SERVER_CONFIG.themoviedb_api_config.language;
+
+  let apiResult = JSON.parse(
+    HTTP.call(
+      'GET', 
+      baseurl + 'discover/movie?api_key=' + apikey + '&language=' + language
+    ).content
+  )
+
+  res.writeHead(200); 
+  res.end(JSON.stringify(apiResult));
 });
