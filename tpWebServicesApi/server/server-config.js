def1dffe1948c46b1eac35ce4fca313870829779
bentarr <benetierpaul@gmail.com>
2021-04-07 21:49:35 +0200

    Delete server-config.js

--- tpWebServicesApi/server/server-config.js
+++
@@ -1,7 +0,0 @@
-export const SERVER_CONFIG ={
-    "themoviedb_api_config" : {
-        "base_url": "https://api.themoviedb.org/3/",
-        "api_key":"1793c4843a64fbd6fdba88ce08e45c5f", 
-        "language" : "fr-FR"
-    }
-}
