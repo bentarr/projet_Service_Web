051194a1d587fbe0adf63ef786c151ef2cb36758
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2021-04-14 00:03:50 +0200

    Récupération du genre presque fonctionnel

--- tpWebServicesApi/client/main.js
+++ tpWebServicesApi/client/main.js
@@ -1,50 +1,94 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { HTTP } from 'meteor/http'; 
+import { HTTP } from 'meteor/http';
 
 import './main.html';
 
+const movies = new ReactiveVar();
+const genres = new ReactiveVar();
+
 Template.home.onCreated(function homeOnCreated() {
-  let ctrl = this;
-  this.movies = new ReactiveVar();
-  
   HTTP.call(
     'GET',
-    'http://localhost:3000/api/discover/movies', 
-  {},
-  (error, response) => {
-    ctrl.movies.set(
-      JSON.parse(response.content).results)
+    'http://localhost:3000/api/discover/movies',
+    {},
+    (error, response) => {
+      movies.set(
+        JSON.parse(response.content).results)
+    }
+  );
+  HTTP.call(
+    'GET',
+    'http://localhost:3000/api/genre/movie/list',
+    {},
+    (error, response) => {
+      genres.set(
+        JSON.parse(response.content).genres)
     }
   );
 });
 
-  Template.home.helpers({
+Template.home.helpers({
   movies() {
-  return Template.instance().movies.get()
+    return movies.get()
+  },
+  genres() {
+    return genres.get()
   }
 });
 
 Template.home.events({
-  'click button'(event, instance) {
+  'click #like'(event, instance) {
     const idMovie = event.currentTarget.dataset.id;
-    updateLikeMovie(idMovie, Template.instance().movies);
+    updateLikeMovie(idMovie, movies);
+  },
+  'click #popu'(event, instance) {
+    getMostPopu();
+  },
+  'submit .genres'(event, instance) {
+    const target = event.target;
+    const idGenre = target.genres.value;
+    getMoviesFromGenre(idGenre);
   }
 });
 
 
 function updateLikeMovie(idMovie, movies) {
-HTTP.call(
-  'PUT',
-  'http://localhost:3000/api/like/' + idMovie,
-  {},
-  (error, response) => {
-    let index = movies.get().findIndex(
+  HTTP.call(
+    'PUT',
+    'http://localhost:3000/api/like/' + idMovie,
+    {},
+    (error, response) => {
+      let index = movies.get().findIndex(
         (item) => { return item.id === JSON.parse(response.content).id; }
-    );
-    let moviesList = movies.get();
-    moviesList[index].like = JSON.parse(response.content).like;
-    movies.set(moviesList);
-  }
-)
+      );
+      let moviesList = movies.get();
+      moviesList[index].like = JSON.parse(response.content).like;
+      movies.set(moviesList);
+    }
+  )
+}
+
+function getMostPopu() {
+  HTTP.call(
+    'GET',
+    'http://localhost:3000/api/discover/most_popu',
+    {},
+    (error, response) => {
+      movies.set(JSON.parse(response.content).results);
+    }
+  )
+}
+
+function getMoviesFromGenre(idGenre) {
+  console.log(idGenre);
+  HTTP.call(
+    'POST',
+    'http://localhost:3000/api/genre/movie/list',
+    {},
+    (error, response) => {
+      genres.set(
+        JSON.parse(response.content).results)
+    }
+  );
 }
--- tpWebServicesApi/server/main.js
+++ tpWebServicesApi/server/main.js
@@ -2,63 +2,113 @@ import { Meteor } from 'meteor/meteor';
 import { SERVER_CONFIG } from './server-config.js';
 import { WebApp } from 'meteor/webapp';
 import { HTTP } from 'meteor/http';
-import {Mongo} from 'meteor/mongo';
+import { Mongo } from 'meteor/mongo';
 
-Meteor.startup(() => {});
+Meteor.startup(() => { });
 
 const Like = new Mongo.Collection('like');
 
+const baseurl = SERVER_CONFIG.themoviedb_api_config.base_url;
+const apikey = SERVER_CONFIG.themoviedb_api_config.api_key;
+const language = SERVER_CONFIG.themoviedb_api_config.language;
 
+//Route de base pour la liste des films
 WebApp.connectHandlers.use('/api/discover/movies', (req, res, next) => {
-  let baseurl = SERVER_CONFIG.themoviedb_api_config.base_url;
-  let apikey = SERVER_CONFIG.themoviedb_api_config.api_key;
-  let language = SERVER_CONFIG.themoviedb_api_config.language;
   HTTP.call(
-    'GET', 
+    'GET',
     baseurl + 'discover/movie?api_key=' + apikey + '&language=' + language,
     {},
     (error, response) => {
       let retour = response.data;
-      retour.results.forEach(element => {
-        let retourReq = Like.findOne({id: element.id});
-        element.like = retourReq ? retourReq.like : 0;
-      });
+      insertLikeBdd(retour);
       res.writeHead(200);
       res.end(JSON.stringify(retour))
     }
   )
 });
 
-
-  WebApp.connectHandlers.use('/api/like', (req,res, next) => {
-    switch(req.method) {
-      case 'GET':
-        break;
-      case 'PUT':
-        let idMovie = req.url.split('/')[1];
-        let newMoviesLikes = updateLikeMovie(parseInt(idMovie));
-        res.writeHead(200);
-        res.end(JSON.stringify(newMoviesLikes));
-        break;
-      default:
-        break;
+//Route pour les films les plus populaires
+WebApp.connectHandlers.use('/api/discover/most_popu', (req, res, next) => {
+  HTTP.call(
+    'GET',
+    baseurl + 'discover/movie?sort_by=vote_average.desc&vote_count.gte=50&api_key=' + apikey + '&language=' + language,
+    {},
+    (error, response) => {
+      let retour = response.data;
+      insertLikeBdd(retour);
+      res.writeHead(200);
+      res.end(JSON.stringify(retour))
     }
-  }
-  );
+  )
+});
 
-  function updateLikeMovie(idMovie) {
-    let ressource = Like.findOne({ id: idMovie });
-    if (ressource) {
-      Like.update(
-        { id: idMovie },
-        { $inc: { like: 1 } }
-      );
-    }else{
-      Like.insert(
-        { id: idMovie,
-        like: 1}
-      );
-    }
-    return Like.findOne({ id: idMovie });
+//Route pour récupérer les genres
+WebApp.connectHandlers.use('/api/genre/movie/list', (req, res, next) => {
+  switch (req.method) {
+    case 'GET':
+      HTTP.call(
+        'GET',
+        baseurl + 'genre/movie/list?api_key=' + apikey + '&language=' + language,
+        {},
+        (error, response) => {
+          let retour = response.data;
+          res.writeHead(200);
+          res.end(JSON.stringify(retour))
+        }
+      )
+    case 'POST':
+      console.log(req);
+      HTTP.call(
+        'POST',
+        baseurl + 'genre/movie/list?api_key=' + apikey + '&language=' + language,
+        {},
+        (error, response) => {
+          let retour = response.data;
+          res.writeHead(200);
+          res.end(JSON.stringify(retour))
+        }
+      )
+    default:
+      break;
   }
+});
 
+WebApp.connectHandlers.use('/api/like', (req, res, next) => {
+  switch (req.method) {
+    case 'GET':
+      break;
+    case 'PUT':
+      let idMovie = req.url.split('/')[1];
+      let newMoviesLikes = updateLikeMovie(parseInt(idMovie));
+      res.writeHead(200);
+      res.end(JSON.stringify(newMoviesLikes));
+      break;
+    default:
+      break;
+  }
+});
+
+function updateLikeMovie(idMovie) {
+  let ressource = Like.findOne({ id: idMovie });
+  if (ressource) {
+    Like.update(
+      { id: idMovie },
+      { $inc: { like: 1 } }
+    );
+  } else {
+    Like.insert(
+      {
+        id: idMovie,
+        like: 1
+      }
+    );
+  }
+  return Like.findOne({ id: idMovie });
+}
+
+function insertLikeBdd(retour) {
+  retour.results.forEach(element => {
+    let retourReq = Like.findOne({ id: element.id });
+    element.like = retourReq ? retourReq.like : 0;
+  });
+}
